0f13ef98de39d9dbd74179d0cd8a9668355fe55b
Danny Lockard <danny.lockard@banno.com>
2018-11-02 13:18:01 -0500

    Making code better formatted

--- extension.js
+++ extension.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = function(nodecg) {
-	var timeLeftRep = nodecg.Replicant('timeleft');
-	var startedRep = nodecg.Replicant('started');
+  var timeLeftRep = nodecg.Replicant('timeleft');
+  var startedRep = nodecg.Replicant('started');
 
 
-	function tick(){
-		if (timeLeftRep.value - 1 > -1) {
-			timeLeftRep.value = timeLeftRep.value - 1;
-			setTimeout(tick, 1000);
-		}
-	}
-	startedRep.on('change', (newValue, oldValue) => {
-		if (newValue) {
-			tick();
-		}
-	});
+  function tick() {
+    if (timeLeftRep.value - 1 > -1) {
+      timeLeftRep.value = timeLeftRep.value - 1;
+      setTimeout(tick, 1000);
+    }
+  }
+  startedRep.on('change', (newValue, oldValue) => {
+    if (newValue) {
+      tick();
+    }
+  });
 
 }
